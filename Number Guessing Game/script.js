da9c5c86ac33534a2db3ea91933b17026789e31d
mjcoronis <124647054+mjcoronis@users.noreply.github.com>
2023-06-03 12:40:03 -0700

    Add files via upload

---
+++ Number Guessing Game/script.js
@@ -0,0 +1,8 @@
+/*Get DOM elements & initialize the game*/
+const input = document.querySelector("input"), 
+guess = document.querySelector(".hint"), 
+checkBtn = document.querySelector("button"),
+remainChances = document.querySelector(".chances");
+
+/* sets the focus on the input field */
+input.focus();
